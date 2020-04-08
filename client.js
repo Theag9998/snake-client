68c2b29da658bf4adcda1f6aab0db0c9cf597869
Thea Ganden <wtp9998@hotmail.com>
2020-04-08 21:49:38 +0000

    refactoring snake

---
+++ client.js
@@ -0,0 +1,19 @@
+const net = require('net');
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '50.64.116.162',
+		port: 50541
+		// 50.64.116.162
+	});
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+	conn.on('data', (data) => {
+		console.log('Server says: ', data);
+	});
+	conn.on("connect", () => {
+		console.log('connected!');
+	})
+
+  return conn;
+}
+module.exports = {connect, };
--- play.js
+++ play.js
@@ -1,25 +1,7 @@
-const net = require('net');
-
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '50.64.116.162',
-		port: 50541
-		// 50.64.116.162
-	});
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-	conn.on('data', (data) => {
-		console.log('Server says: ', data);
-	});
-	conn.on("connect", () => {
-		console.log('connected!');
-	})
 
-  return conn;
-}
-
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
